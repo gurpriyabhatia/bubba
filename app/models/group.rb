442823207d5516199a7a5c77382e3640a311a9a7
Francois Baud <fxbaud@gmail.com>
2020-02-20 21:28:37 +0000

    with checks with rails c

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   has_one :group_picture
   belongs_to :users
-  validates :descriptionm, presence: true
+  validates :description, presence: true
   validates :address, presence: true
   validates :location, presence: true
   geocoded_by :address

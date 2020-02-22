46aeda3772e0532669cb3aaab53a6d069f057aab
Alexia Tanguy alexia.tanguy1@gmail.com <alexia.tanguy1@gmail.com>
2020-02-22 11:45:32 +0000

    nanny_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_02_20_212110) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-  create_table "nannies", force: :cascade do |t|
-    t.string "name"
-    t.string "bio"
-    t.string "address"
-    t.integer "range_in_miles"
-    t.string "mobile_number"
-    t.integer "price_per_hour"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_nannies_on_user_id"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -60,7 +45,19 @@ ActiveRecord::Schema.define(version: 2020_02_20_212110) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+  end
 
+  create_table "nannies", force: :cascade do |t|
+    t.string "name"
+    t.string "bio"
+    t.string "address"
+    t.integer "range_in_miles"
+    t.string "mobile_number"
+    t.integer "price_per_hour"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_nannies_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -75,9 +72,6 @@ ActiveRecord::Schema.define(version: 2020_02_20_212110) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "nannies", "users"
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
+  add_foreign_key "nannies", "users"
 end

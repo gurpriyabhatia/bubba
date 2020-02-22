d3d4f39f29755a7876c3e5feba58e871f7893b51
Rachel Clarke <clarkerachel21@gmail.com>
2020-02-22 11:32:08 +0000

    created event routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :groups
+  resources :groups do
+    resources :events
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

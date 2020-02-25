79bee22e7e74b46ec7120e5fbef06c9174f6b867
Rachel Clarke <clarkerachel21@gmail.com>
2020-02-25 19:18:32 +0000

    commented out redundant Event method

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,10 +7,11 @@ class Event < ApplicationRecord
   validates :price, numericality: true
   validates :availability, numericality: true, allow_nil: true
 
-  def add_attendee
-    return if availability.zero?
+  # TODO: Method for when a user books an event??
+  # def add_attendee
+  #   return if availability.zero?
 
-    new_availability = self.availability -= 1
-    update(availability: new_availability)
-  end
+  #   new_availability = self.availability -= 1
+  #   update(availability: new_availability)
+  # end
 end

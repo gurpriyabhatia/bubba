56b37f9c0a3ef82f436d2a35c40187da95ec015b
Alexia Tanguy alexia.tanguy1@gmail.com <alexia.tanguy1@gmail.com>
2020-02-29 14:17:51 +0000

    take care with this commit

---
+++ app/javascript/packs/init_mapboxnannies.js
@@ -0,0 +1,55 @@
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+const mapElement = document.getElementById('mapnannies');
+
+const buildMap = () => {
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  return new mapboxgl.Map({
+    container: 'mapnannies',
+    style: 'mapbox://styles/mapbox/streets-v11'
+  });
+};
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.nannyimage_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '30px';
+    element.style.height = '30px';
+
+    new mapboxgl.Marker(element)
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+  });
+};
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 25, maxZoom: 15 });
+};
+
+const initMapboxNannies = () => {
+  if (mapElement) {
+    const map = buildMap();
+    const markers = JSON.parse(mapElement.dataset.markers);
+
+    addMarkersToMap(map, markers);
+    fitMapToMarkers(map, markers);
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                      mapboxgl: mapboxgl }));
+  }
+};
+
+setTimeout(function(){ document.querySelector(".mapboxgl-ctrl-geocoder--input").remove();
+ document.querySelector(".mapboxgl-ctrl-geocoder--icon-search").remove();    }, 50);
+
+
+
+export { initMapboxNannies };

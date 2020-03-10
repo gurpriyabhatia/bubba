1eaeabe291011589784bd809294ae2465c5e379f
Francois Baud <fxbaud@gmail.com>
2020-03-10 21:36:50 +0000

    update links with the bubba-new color

--- app/javascript/packs/init_mapbox.js
+++ app/javascript/packs/init_mapbox.js
@@ -40,7 +40,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 15, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 15, maxZoom: 20 });
 };
 
 const initMapbox = () => {
